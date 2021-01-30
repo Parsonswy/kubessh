3b56ce02cfc021728ab7f55da013f33203780aca
Wyatt Parsons <wdparson@ncsa.tech>
2021-01-29 23:16:38 -0500

    Add LDAP(s) authentication support

--- kubessh_dummy_config.py
+++ kubessh_dummy_config.py
@@ -1,7 +1,7 @@
-from kubessh.authentication.dummy import DummyAuthenticator
+from kubessh.authentication.ldap import LDAPAuthenticator
 
 # Make sure this exists
 c.KubeSSH.host_key_path = 'dummy-kubessh-host-key'
 
 c.KubeSSH.debug = True
-c.KubeSSH.authenticator_class = DummyAuthenticator
+c.KubeSSH.authenticator_class = LDAPAuthenticator
